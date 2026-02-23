e0a885bdafa74287e8149da1c104429a88aa448f
William <williamlarsen210@gmail.com>
2026-02-23 16:56:50 +0100

    Update MultiplierDTO.java

--- src/main/java/dk/aau/network_management_system/multiplier/MultiplierDTO.java
+++ src/main/java/dk/aau/network_management_system/multiplier/MultiplierDTO.java
@@ -1,10 +1,6 @@
 package dk.aau.network_management_system.multiplier;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-// gør at klienten kun kan sende de nederse 3 
 // skal ændres til kun at være material og muliplier 
 // cooperative id skal vi gerne have fra man logger ind
 public class MultiplierDTO {
