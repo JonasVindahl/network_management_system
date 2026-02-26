4bd08b02cb4b257a1dfec951d5dfd791503c11ca
Jonas Bang <github@jonasvindahl.dev>
2026-02-26 09:44:29 +0100

    Added a /test.html for saving tokens, so its easier to test

--- src/main/java/dk/aau/network_management_system/auth/SecurityConfig.java
+++ src/main/java/dk/aau/network_management_system/auth/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/test.html", "/*.html", "/static/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
