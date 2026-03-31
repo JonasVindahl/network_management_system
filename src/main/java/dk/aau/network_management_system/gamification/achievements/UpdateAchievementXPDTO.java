01fb7c33e7089ae4003da0e08515abb227a7605d
William <williamlarsen210@gmail.com>
2026-03-31 14:06:24 +0200

    Fixed package

--- src/main/java/dk/aau/network_management_system/gamification/achievements/UpdateAchievementXPDTO.java
+++ src/main/java/dk/aau/network_management_system/gamification/achievements/UpdateAchievementXPDTO.java
@@ -1,4 +1,4 @@
-package dk.aau.network_management_system.gamification; 
+package dk.aau.network_management_system.gamification.achievements; 
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
